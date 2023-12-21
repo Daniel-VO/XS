df514d0ca0c0bbf5b8fcc85cb563daf2039440e4
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2023-12-21 07:48:15 +0100

    Add files via upload

--- 2D-RSS_datacollect.py
+++ 2D-RSS_datacollect.py
@@ -1,5 +1,5 @@
 """
-Created 20. December 2023 by Daniel Van Opdenbosch, Technical University of Munich
+Created 21. December 2023 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -11,21 +11,21 @@ import numpy as np
 
 @ray.remote
 def collect(f):
-	chi_collect,phi_collect,twotheta_collect,yobs_collect=[],[],[],[]
+	chi_collect,twotheta_collect,yobs_collect=[],[],[]
 	for line in open(f,'r').readlines():
 		if line.split()[0]=='*MEAS_COND_AXIS_POSITION-2':
 			chi=float(line.split()[1].replace('"',''))
 		if line.split()[0]=='*MEAS_COND_AXIS_POSITION-5':
 			phi=float(line.split()[1].replace('"',''))
 		if '*' not in line:
-			chi_collect.append(chi)#;phi_collect.append(phi)
+			chi_collect.append(chi)
 			twotheta_collect.append(float(line.split()[0]))
 			yobs_collect.append(float(line.split()[1]))
+	print(phi)
 	yobs,chi,twotheta=np.histogram2d(chi_collect,twotheta_collect,weights=yobs_collect,\
 					  bins=[np.arange(min(chi_collect),max(chi_collect)),\
-							np.arange(min(twotheta_collect),max(twotheta_collect))])
-	chi,twotheta=np.meshgrid(chi[1:],twotheta[1:]);phi=np.full(yobs.shape,phi)
-	print(phi)
+							np.arange(min(twotheta_collect),max(twotheta_collect),step=0.1)])
+	chi,twotheta=np.meshgrid(chi[1:],twotheta[1:]);phi=np.full(yobs.shape,phi);yobs=yobs.transpose()
 	return chi.flatten(),phi.flatten(),twotheta.flatten(),yobs.flatten()
 
 chi,phi,twotheta,yobs=np.concatenate(ray.get([collect.remote(f) for f in glob.glob('*.ras')]),axis=1)
