c98024a4c9d4f1f87e8dc9a10d41314e6aed502a
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-01-03 11:09:15 +0100

    Add files via upload

--- 2D-RSS_refindex.py
+++ 2D-RSS_refindex.py
@@ -1,5 +1,5 @@
 """
-Created 21. December 2023 by Daniel Van Opdenbosch, Technical University of Munich
+Created 03. January 2023 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -11,8 +11,12 @@ from crystals import Atom,Crystal
 
 q,qx,qy,qz,yobs,sigq=np.load('reflections.npy')
 
+r=scipy.spatial.transform.Rotation.from_euler('z',45,degrees=True)
+for i,valuei in enumerate(q):
+	qx[i],qy[i],qz[i]=r.apply([qx[i],qy[i],qz[i]])
+
 guess=crystals.lattice.Lattice.from_parameters(a=5.43070,b=5.43070,c=5.43070,alpha=90.00,beta=90.00,gamma=90.00)
-lattice=crystals.index_dirax(np.array([qx,qy,qz]).transpose(),initial=None)
+lattice=crystals.index_dirax(np.array([qx,qy,qz]).transpose(),initial=guess,length_bounds=(2,3))
 
 plt.close('all')
 ax=plt.figure().add_subplot(projection='3d')
