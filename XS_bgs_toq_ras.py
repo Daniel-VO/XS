c8bce9d60722fd5f455f5a659d8c334bce38db6d
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-03-07 14:46:12 +0100

    Add files via upload

--- XS_bgs_toq_ras.py
+++ XS_bgs_toq_ras.py
@@ -35,13 +35,18 @@ for p in paths:
 			ttbg,ybg,epsbg=np.genfromtxt((i.replace('*','#') for i in open(BGfiles[0])),unpack=True)
 			qbg=toq(ttbg)														#to q
 			qbg=zdc(qbg,ybg)													#zero drift correction
-			argsgauss=np.where((qbg>=min(qbg))&(qbg<=-min(qbg)))
-			popt,pcov=scipy.optimize.curve_fit(gaussian,qbg[argsgauss],ybg[argsgauss],p0=[max(ybg),0,1e-4])
-			HWHM=(2*np.log(2))**0.5*popt[-1]									#HWHM
+			if s=='*_USAXS' or s=='*_SAXS':
+				argsgauss=np.where((qbg>=min(qbg))&(qbg<=-min(qbg)))
+				popt,pcov=scipy.optimize.curve_fit(gaussian,qbg[argsgauss],ybg[argsgauss],p0=[max(ybg),0,1e-4])
+				HWHM=(2*np.log(2))**0.5*popt[-1]									#HWHM
+			else:
+				HWHM=0
+			print('qy_width = '+str(HWHM)+' A^-1')
 			bg=scipy.interpolate.interp1d(qbg,ybg)								#bgint
 
 		for f in glob.glob('[!BG]'+p+s+'.ras'):
 			filename=os.path.splitext(f)[0]
+			print(filename)
 			tt,yobs,eps=np.genfromtxt((i.replace('*','#') for i in open(f)),unpack=True)
 			q=toq(tt)															#to q
 			q=zdc(q,yobs)														#zero drift correction
@@ -49,7 +54,7 @@ for p in paths:
 			q=q[argscut];yobs=yobs[argscut]										#cut
 			ybg=bg(q)
 			yobs-=ybg															#bgcorr
-			print(filename,'qy_width = '+str(HWHM)+' A^-1')
+
 
 			plt.close('all')
 			if len(BGfiles)==1:
@@ -65,7 +70,7 @@ for p in paths:
 			plt.xscale('log'),plt.yscale('log'),plt.xlim([[1e-4,1e-3,1e-2][int(np.where(np.array(['*_USAXS','*_SAXS','*_TXRD'])==s)[0][0])],None]),plt.ylim([None,2*max(yobs)])
 			plt.savefig(filename+'.png')
 
-			with open(filename+'_bgs_toq.dat','a') as f:
-				f.write('#qy_width = '+str(HWHM)+'\n')
-				np.savetxt(f,np.transpose([q,yobs]),fmt='%.16f')
+			with open(filename+'_bgs_toq.dat','a') as d:
+				d.write('#qy_width = '+str(HWHM)+'\n')
+				np.savetxt(d,np.transpose([q,yobs]),fmt='%.16f')
 
