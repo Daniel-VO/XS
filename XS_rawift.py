2ef165ac5e0e3f72bb37c142bd975dcb54e367db
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-03-12 13:46:46 +0100

    Add files via upload

---
+++ XS_rawift.py
@@ -0,0 +1,40 @@
+"""
+Created 08. March 2024 by Daniel Van Opdenbosch, Technical University of Munich
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
+"""
+
+import os
+import sys
+import glob
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import bioxtasraw.RAWAPI as raw
+
+for f in glob.glob('*_stitch.dat'):
+	filename=os.path.splitext(f)[0]
+	q,yobs=np.genfromtxt(f,unpack=True)
+
+	q,yobs=q[np.argmax(yobs):],yobs[np.argmax(yobs):]							####
+
+	plt.close('all')
+	plt.plot(q,yobs)
+
+	args=np.where((q>=0.1)&(q<=10))											####
+
+	profile=raw.make_profile(q[args],yobs[args],np.ones(len(args[0])),'profile')
+	guinier_results=raw.auto_guinier(profile)
+	ift=raw.bift(profile)
+
+	plt.plot(ift[0].q_orig,ift[0].i_orig)
+	plt.plot(ift[0].q_orig,ift[0].i_fit)
+	plt.xscale('log');plt.yscale('log')
+	plt.savefig(filename+'_iq.png')
+
+	plt.close('all')
+	plt.plot(ift[0].r,ift[0].p)
+	plt.savefig(filename+'_pr.png')
+
+	raw.save_ift(ift[0],filename+'.ift','./')
+	raw.save_report(filename+'.pdf','./',[profile],[ift[0]])
