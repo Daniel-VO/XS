1928949e7171e73a4a949869e1cc5d239a071b4f
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-03-20 09:57:39 +0100

    Add files via upload

--- XS_rawift.py
+++ XS_rawift.py
@@ -1,5 +1,5 @@
 """
-Created 08. March 2024 by Daniel Van Opdenbosch, Technical University of Munich
+Created 20. March 2024 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -12,11 +12,13 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import bioxtasraw.RAWAPI as raw
 
-ranges=[[1e-3,1e-1],
-		[1e-1,1e1]]
+ranges=[
+		[1e-3,4e-2],
+		[4e-2,7e-1],
+		[7e-1,1e1]
+		]
 
-profiles=[];ifts=[]
-
+os.system('mv results.log results.alt')
 for r in ranges:
 	for f in glob.glob('*_stitch.dat'):
 		filename=os.path.splitext(f)[0]+'_'+str(r)
@@ -30,9 +32,8 @@ for r in ranges:
 		args=np.where((q>=r[0])&(q<=r[1]))										####
 
 		profile=raw.make_profile(q[args],yobs[args],np.ones(len(args[0])),filename)
-		guinier_results=raw.auto_guinier(profile)
+		guinier=raw.auto_guinier(profile)
 		ift=raw.bift(profile)
-		profiles.append(profile);ifts.append(ift[0])
 
 		plt.plot(ift[0].q_orig,ift[0].i_orig)
 		plt.plot(ift[0].q_orig,ift[0].i_fit)
@@ -44,5 +45,5 @@ for r in ranges:
 		plt.savefig(filename+'_pr.png')
 
 		raw.save_ift(ift[0],filename+'.ift')
-
-raw.save_report('report.pdf',profiles=profiles,ifts=ifts)
+		raw.save_report(filename+'.pdf',profiles=[profile],ifts=[ift[0]])
+		print(filename,'rg =',guinier[0],'+-',guinier[2],' bift_rg =',ift[2],'+-',ift[5],file=open('results.log','a'))
