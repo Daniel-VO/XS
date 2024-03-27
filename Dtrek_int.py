06f76dbaf587223f72bc855b11af535f8e12b25e
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-03-27 13:13:35 +0100

    Add files via upload

--- Dtrek_int.py
+++ Dtrek_int.py
@@ -1,5 +1,5 @@
 """
-Created 17. Januar 2023 by Daniel Van Opdenbosch, Technical University of Munich
+Created 27. March 2024 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
