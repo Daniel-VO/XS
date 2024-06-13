7369b5bcfd5f985f68ae35a5cef2bbcea1ad3966
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-06-13 15:47:26 +0200

    Add files via upload

--- Dtrek_int.py
+++ Dtrek_int.py
@@ -1,21 +1,28 @@
 """
-Created 06. June 2024 by Daniel Van Opdenbosch, Technical University of Munich
+Created 10. June 2024 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
 
 import os
+import sys
 import glob
 import fabio
 import pyFAI
-import pygix
 import scipy
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from pyFAI import azimuthalIntegrator as pa
-from pygix import lattice as pl
-from pygix import plotting as pp
+
+if len(sys.argv)>1:
+	geom=sys.argv[1]
+else:
+	geom=input('Geometrie [Faser/GISAXS/ ]: ')
+
+
+def label(string):
+	return r'$'+string.replace('^-1','^{-1}').replace('_deg','/^\circ').replace('A',r'\rm{A}').replace('nm',r'\rm{nm}').replace('_','}/').replace('q','q_{').replace('th',r'\theta')+'$'
 
 def take(img,headerkey,indices):
 	return np.fromstring(img.header[headerkey],sep=' ')[indices]
@@ -23,77 +30,30 @@ def take(img,headerkey,indices):
 for f in glob.glob('*.img'):
 	img=fabio.open(f);filename=os.path.splitext(f)[0].replace('_image','')
 	detdist=take(img,'PXD_GONIO_VALUES',-1);beamcenterX,beamcenterY,pxsizeX,pxsizeY=take(img,'PXD_SPATIAL_DISTORTION_INFO',[0,1,2,3]);wavelength=take(img,'SOURCE_WAVELENGTH',-1);omega,chi,phi=take(img,'CRYSTAL_GONIO_VALUES',[0,1,2])
-	ai=pa.AzimuthalIntegrator(
-	dist=detdist/1e3,poni1=beamcenterY*pxsizeY/1e3,poni2=beamcenterX*pxsizeX/1e3,pixel1=pxsizeY/1e3,pixel2=pxsizeX/1e3,wavelength=wavelength/1e10)
-	pg=pygix.Transform(
-	dist=detdist/1e3,poni1=beamcenterY*pxsizeY/1e3,poni2=beamcenterX*pxsizeX/1e3,pixel1=pxsizeY/1e3,pixel2=pxsizeX/1e3,wavelength=wavelength/1e10,incident_angle=0,sample_orientation=1,tilt_angle=0,useqx=True)
-	npts=max(img.shape)//2
+	ai=pa.AzimuthalIntegrator(dist=detdist/1e3,poni1=beamcenterY*pxsizeY/1e3,poni2=beamcenterX*pxsizeX/1e3,pixel1=pxsizeY/1e3,pixel2=pxsizeX/1e3,wavelength=wavelength/1e10)
 
-	#AI
-	plt.close('all')
-	plt.figure(figsize=(7.5/2.54,5.3/2.54))
-	mpl.rc('text',usetex=True);mpl.rc('text.latex',preamble=r'\usepackage[helvet]{sfmath}')
-	yobs,q,chi=ai.integrate2d(img.data,npt_rad=npts,npt_azim=npts)
-	plt.pcolormesh(q,chi,yobs,cmap='coolwarm')
-	plt.xlabel(r'$q/\rm{nm}^{-1}$')
-	plt.ylabel(r'$\chi/^\circ$')
-	plt.tick_params(axis='both',pad=2,labelsize=8)
-	plt.tight_layout(pad=0.1)
-	plt.savefig(filename+'_ai2d.png',dpi=300)
-
-	p1_ranges=[(-10,10),(80,100)]
-	for p1_range in p1_ranges:
-		plt.close('all')
-		plt.figure(figsize=(7.5/2.54,5.3/2.54))
-		mpl.rc('text',usetex=True);mpl.rc('text.latex',preamble=r'\usepackage[helvet]{sfmath}')
-		q,yobs=ai.integrate1d(img.data,npt=npts,azimuth_range=p1_range)
-		plt.plot(q,yobs/max(yobs))
-		plt.figtext(0.98,0.98,r'$\chi='+str((p1_range[0],p1_range[1]))+'$',ha='right',va='top',fontsize=6)
-		plt.xlabel(r'$q/\rm{nm}^{-1}$')
-		plt.ylabel(r'$I/1$')
-		plt.tick_params(axis='both',pad=2,labelsize=8)
-		plt.tight_layout(pad=0.1)
-		plt.savefig(filename+'_'+str(p1_range)+'_aiIq.png',dpi=300)
-
-	#PG
-	plt.close('all')
-	plt.figure(figsize=(7.5/2.54,5.3/2.54))
-	mpl.rc('text',usetex=True);mpl.rc('text.latex',preamble=r'\usepackage[helvet]{sfmath}')
-	yobs,q,chi=pg.transform_polar(img.data,npt=(npts,npts),method='bbox')
-	qg,chig=np.meshgrid(q,chi);yobs[np.where(abs(chig)==np.min(abs(chig)))]=0
-	plt.pcolormesh(q,chi,yobs,cmap='coolwarm')
-	plt.xlabel(r'$q/\rm{nm}^{-1}$')
-	plt.ylabel(r'$\chi/^\circ$')
-	plt.tick_params(axis='both',pad=2,labelsize=8)
-	plt.tight_layout(pad=0.1)
-	plt.savefig(filename+'_pg2d.png',dpi=300)
+	if geom=='Faser':
+		units=('qip_A^-1','qoop_A^-1');method='no';npts=(img.shape[1]//2,img.shape[0]//2)
+	else:
+		units='q_A^-1';method='splitpixel';npts=(img.shape[1]//2,360)
 
 	plt.close('all')
 	plt.figure(figsize=(7.5/2.54,5.3/2.54))
 	mpl.rc('text',usetex=True);mpl.rc('text.latex',preamble=r'\usepackage[helvet]{sfmath}')
-	yobs,qxy,qz=pg.transform_reciprocal(img.data,npt=(npts,npts),method='bbox')
-	qxyg,qzg=np.meshgrid(qxy,qz);args=np.where(abs(qxyg)==np.min(abs(qxyg)))
-	yobs[args]=np.median([yobs[args[0],args[1]-1],yobs[args[0],args[1]+1]],axis=0)
-	plt.pcolormesh(qxy,qz,yobs,cmap='coolwarm')
-	plt.axis('equal')
-	plt.xlabel(r'$q_{xy}/\rm{nm}^{-1}$')
-	plt.ylabel(r'$q_z/\rm{nm}^{-1}$')
+	yobs,x,y=ai.integrate2d(img.data,npts[0],npts[1],unit=units,method=method)
+
+	if geom=='Faser':
+		xg,yg=np.meshgrid(x,y);args=np.where(abs(xg)==np.min(abs(xg)))
+		yobs[args]=np.median([yobs[args[0],args[1]-1],yobs[args[0],args[1]+1]],axis=0)
+		xlabel=label(units[0]);ylabel=label(units[1])
+	else:
+		xlabel=label(units);ylabel=r'$\chi/^\circ$'
+
+	plt.pcolormesh(x,y,yobs,cmap='coolwarm')
+	plt.xlabel(xlabel);plt.ylabel(ylabel)
 	plt.tick_params(axis='both',pad=2,labelsize=8)
 	plt.tight_layout(pad=0.1)
-	plt.savefig(filename+'_pgrs.png',dpi=300)
+	plt.savefig(filename+'.png',dpi=300)
 
-	qz_props=[(0.1,-0.1),(-0.1,0.1)]
-	for qz_prop in qz_props:
-		plt.close('all')
-		plt.figure(figsize=(7.5/2.54,5.3/2.54))
-		mpl.rc('text',usetex=True);mpl.rc('text.latex',preamble=r'\usepackage[helvet]{sfmath}')
-		qg=(qxyg**2+qzg**2)**0.5
-		args=np.where((qzg/qg>=qz_prop[0])&(qzg/qg<=qz_prop[1]))
-		yint,q=np.histogram(qg[args].flatten(),weights=yobs[args].flatten(),bins=npts//2)
-		plt.plot(q[1:],yint/np.nan_to_num(np.max(yint)))
-		plt.figtext(0.98,0.98,r'$q_z/q='+str((qz_prop[0],qz_prop[1]))+'$',ha='right',va='top',fontsize=6)
-		plt.xlabel(r'$q/\rm{nm}^{-1}$')
-		plt.ylabel(r'$I/1$')
-		plt.tick_params(axis='both',pad=2,labelsize=8)
-		plt.tight_layout(pad=0.1)
-		plt.savefig(filename+'_'+str(qz_prop)+'_pgIq.png',dpi=300)
+
+
