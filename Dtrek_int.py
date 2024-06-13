e927b332ce034f24dcdc9e24c7bdf69aa120f704
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-06-13 16:28:18 +0200

    Add files via upload

--- Dtrek_int.py
+++ Dtrek_int.py
@@ -20,7 +20,6 @@ if len(sys.argv)>1:
 else:
 	geom=input('Geometrie [Faser/GISAXS/ ]: ')
 
-
 def label(string):
 	return r'$'+string.replace('^-1','^{-1}').replace('_deg','/^\circ').replace('A',r'\rm{A}').replace('nm',r'\rm{nm}').replace('_','}/').replace('q','q_{').replace('th',r'\theta')+'$'
 
@@ -33,19 +32,24 @@ for f in glob.glob('*.img'):
 	ai=pa.AzimuthalIntegrator(dist=detdist/1e3,poni1=beamcenterY*pxsizeY/1e3,poni2=beamcenterX*pxsizeX/1e3,pixel1=pxsizeY/1e3,pixel2=pxsizeX/1e3,wavelength=wavelength/1e10)
 
 	if geom=='Faser':
-		units=('qip_A^-1','qoop_A^-1');method='no';npts=(img.shape[1]//2,img.shape[0]//2)
+		ta=0;so=1
+		unit_qip=pyFAI.units.get_unit_fiber('qip_A^-1',incident_angle=np.radians(omega),tilt_angle=np.radians(ta),sample_orientation=so)
+		unit_qoop=pyFAI.units.get_unit_fiber('qoop_A^-1',incident_angle=np.radians(omega),tilt_angle=np.radians(ta),sample_orientation=so)
+		units=(unit_qip,unit_qoop);method='no';npts=(img.shape[1]//2,img.shape[0]//2)
 	else:
 		units='q_A^-1';method='splitpixel';npts=(img.shape[1]//2,360)
 
+	#2D
 	plt.close('all')
 	plt.figure(figsize=(7.5/2.54,5.3/2.54))
 	mpl.rc('text',usetex=True);mpl.rc('text.latex',preamble=r'\usepackage[helvet]{sfmath}')
+
 	yobs,x,y=ai.integrate2d(img.data,npts[0],npts[1],unit=units,method=method)
 
 	if geom=='Faser':
 		xg,yg=np.meshgrid(x,y);args=np.where(abs(xg)==np.min(abs(xg)))
 		yobs[args]=np.median([yobs[args[0],args[1]-1],yobs[args[0],args[1]+1]],axis=0)
-		xlabel=label(units[0]);ylabel=label(units[1])
+		xlabel=label(str(units[0]));ylabel=label(str(units[1]))
 	else:
 		xlabel=label(units);ylabel=r'$\chi/^\circ$'
 
@@ -55,5 +59,26 @@ for f in glob.glob('*.img'):
 	plt.tight_layout(pad=0.1)
 	plt.savefig(filename+'.png',dpi=300)
 
+	#1D
+	plt.close('all')
+	plt.figure(figsize=(7.5/2.54,5.3/2.54))
+	mpl.rc('text',usetex=True);mpl.rc('text.latex',preamble=r'\usepackage[helvet]{sfmath}')
+
+	if geom=='Faser':
+		rang=None
+		x,yobs=ai.integrate_fiber(img.data,output_unit=units[0],npt_output=npts[0],integrated_unit=units[1],npt_integrated=npts[1],integrated_unit_range=rang,correctSolidAngle=True,method=method)
+		yobs[np.where(abs(x)==min(abs(x)))]=0
+		xlabel=label(str(units[0]));ylabel=r'$I/1$'
+	else:
+		rang=(-180,180)
+		x,yobs=ai.integrate1d(img.data,)
+		xlabel=label(units);ylabel=r'$I/1$'
+
+	plt.plot(x,yobs,'k',linewidth=0.5)
+
+	plt.xlabel(xlabel);plt.ylabel(ylabel)
+	plt.tick_params(axis='both',pad=2,labelsize=8)
+	plt.tight_layout(pad=0.1)
+	plt.savefig(filename+'_'+str(rang)+'.png',dpi=300)
 
 
