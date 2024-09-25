c600ccf55a7ce2421ec8958c5e4cda76b2faca69
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-09-25 10:31:22 +0200

    Add files via upload

--- XS_svfit.py
+++ XS_svfit.py
@@ -1,5 +1,5 @@
 """
-Created 21. June 2024 by Daniel Van Opdenbosch, Technical University of Munich
+Created 25. September 2024 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -20,13 +20,16 @@ from sasmodels.direct_model import call_kernel
 def pad(q):
 	return np.concatenate((np.linspace(min(q)/2,min(q)),q,np.linspace(max(q),max(q)*2)))
 
+def powerlaw(q,plscale,power):
+	return plscale*q**(-power)
+
 def fitfunc(params):
 	prm=params.valuesdict()
 	global res
-	res=((yobsS-SAXSres.apply(call_kernel(SAXSkernel,prm))[50:-50])*qS**2)
+	res=((yobsS-powerlaw(qS,prm['plscale'],prm['power'])-SAXSres.apply(call_kernel(SAXSkernel,prm))[50:-50])*qS**2)
 	if os.path.isfile(f.replace('_SAXS','_USAXS')):
 		prm['scale']=prm['Uscale'];prm['background']=prm['Ubackground']
-		res=np.append(((yobsU-USAXSres.apply(call_kernel(USAXSkernel,prm))[50:-50])*qU**2),res)
+		res=np.append(((yobsU-powerlaw(qU,prm['plscale'],prm['power'])-USAXSres.apply(call_kernel(USAXSkernel,prm))[50:-50])*qU**2),res)
 	return np.nan_to_num(res)
 
 model=load_model(str(sys.argv[1]))
@@ -42,6 +45,12 @@ if str(sys.argv[1])=='unified_power_Rg':
 else:
 	for item in model.info.parameters._get_defaults().items():
 		params.add(item[0],item[1],min=0)
+
+params.add('plscale',0,min=0,vary=False)
+params.add('power',4,min=2,max=6,vary=False)
+
+# ~ params.add('radius_pd',0,min=0,max=1)
+# ~ params.add('radius_pd_n',10,vary=False)
 
 os.system('mv '+model.info.id+'.log '+model.info.id+'.alt')
 
@@ -81,12 +90,12 @@ def fit(g):
 	plt.figure(figsize=(7.5/2.54,5.3/2.54))
 
 	plt.scatter(qS,yobsS,c='k',marker='.',s=2,linewidth=0)
-	plt.plot(qSkernel[50:-50],SAXSres.apply(call_kernel(SAXSkernel,prm))[50:-50],'0.3',linewidth=0.5)
+	plt.plot(qSkernel[50:-50],powerlaw(qS,prm['plscale'],prm['power'])+SAXSres.apply(call_kernel(SAXSkernel,prm))[50:-50],'0.3',linewidth=0.5)
 
 	if os.path.isfile(f.replace('_SAXS','_USAXS')):
 		prm['scale']=prm['Uscale'];prm['background']=prm['Ubackground']
 		plt.scatter(qU,yobsU,c='k',marker='.',s=2,linewidth=0)
-		plt.plot(qUkernel[50:-50],USAXSres.apply(call_kernel(USAXSkernel,prm))[50:-50],'0.3',linewidth=0.5)
+		plt.plot(qUkernel[50:-50],powerlaw(qU,prm['plscale'],prm['power'])+USAXSres.apply(call_kernel(USAXSkernel,prm))[50:-50],'0.3',linewidth=0.5)
 
 	plt.xscale('log');plt.yscale('log')
 
@@ -102,7 +111,7 @@ def fit(g):
 	# ~ plt.xscale('log')
 	# ~ plt.savefig(filename+'_'+model.info.id+'_res.png',dpi=300)
 
-	print(filename,[str((param.name,'=',param.value,'+-',param.stderr)) for param in result.params.values()],file=open(model.info.id+'.log','a'))
+	print(filename,[str((param.name,'=',param.value,'+-',param.stderr)) for param in result.params.values()],np.sum(res**2),file=open(model.info.id+'.log','a'))
 
 	return filename,result
 
