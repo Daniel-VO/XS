e1c468c0009c1980ca04f934ce4ecb89d3d44f7b
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-11-12 08:28:01 +0100

    Add files via upload

--- Dtrek_int.py
+++ Dtrek_int.py
@@ -1,5 +1,5 @@
 """
-Created 23. Juli 2024 by Daniel Van Opdenbosch, Technical University of Munich
+Created 24. Juli 2024 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -15,6 +15,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pybaselines as pb;bl=pb.api.Baseline()
 from pyFAI import azimuthalIntegrator as pa
+from pyFAI.units import get_unit_fiber
 
 if len(sys.argv)>1:
 	geom=sys.argv[1]
@@ -27,15 +28,15 @@ def label(string):
 def take(img,headerkey,indices):
 	return np.fromstring(img.header[headerkey],sep=' ')[indices]
 
-for f in glob.glob('*.img'):
-	img=fabio.open(f);filename=os.path.splitext(f)[0].replace('_image','')
+for f in glob.glob('*[!_BG_image].img'):
+	img=fabio.open(f);filename=os.path.splitext(f)[0].replace('_image','');print(filename)
 	detdist=take(img,'PXD_GONIO_VALUES',-1);beamcenterX,beamcenterY,pxsizeX,pxsizeY=take(img,'PXD_SPATIAL_DISTORTION_INFO',[0,1,2,3]);wavelength=take(img,'SOURCE_WAVELENGTH',-1);omega,chi,phi=take(img,'CRYSTAL_GONIO_VALUES',[0,1,2])
 	ai=pa.AzimuthalIntegrator(dist=detdist/1e3,poni1=beamcenterY*pxsizeY/1e3,poni2=beamcenterX*pxsizeX/1e3,pixel1=pxsizeY/1e3,pixel2=pxsizeX/1e3,wavelength=wavelength/1e10)
 
 	if geom=='Faser':
 		ta=0;so=int(sys.argv[2])
-		unit_qip=pyFAI.units.get_unit_fiber('qip_A^-1',incident_angle=np.radians(omega),tilt_angle=np.radians(ta),sample_orientation=so)
-		unit_qoop=pyFAI.units.get_unit_fiber('qoop_A^-1',incident_angle=np.radians(omega),tilt_angle=np.radians(ta),sample_orientation=so)
+		unit_qip=get_unit_fiber('qip_A^-1',incident_angle=np.radians(omega),tilt_angle=np.radians(ta),sample_orientation=so)
+		unit_qoop=get_unit_fiber('qoop_A^-1',incident_angle=np.radians(omega),tilt_angle=np.radians(ta),sample_orientation=so)
 		units=(unit_qip,unit_qoop);method='no';npts=(img.shape[1]//2,img.shape[0]//2)
 		if so==2 or so==4:
 			npts=npts[::-1]
@@ -43,6 +44,9 @@ for f in glob.glob('*.img'):
 		units='2th_deg';method='splitpixel';npts=(img.shape[1]//2,360)
 
 	#BGCORR
+	if len(glob.glob('*_BG_image.img'))>0:
+		bg=glob.glob('*_BG_image.img')[0];print('Untergrund: '+bg)
+		img.data=img.data.astype('float')-fabio.open(bg).data.astype('float')
 	mfilt1d=ai.medfilt1d(img.data,npt_rad=npts[0],npt_azim=npts[1],unit='2th_deg',method=method,percentile=5)
 	baseline=bl.irsqr(mfilt1d.intensity,diff_order=2)[0]
 	isotropic=ai.calcfrom1d(mfilt1d.radial,baseline,shape=img.shape,dim1_unit=mfilt1d.unit)
@@ -53,6 +57,8 @@ for f in glob.glob('*.img'):
 	int1d=ai.integrate1d(img.data-isotropic,npt=npts[0],unit='2th_deg',method=method);plt.plot(int1d.radial,int1d.intensity,'k--')
 	plt.plot(mfilt1d.radial,mfilt1d.intensity)
 	plt.plot(mfilt1d.radial,baseline)
+	if 'SAXS' in filename:
+		plt.yscale('log')
 	plt.savefig(filename+'_BG1d.png',dpi=300)
 
 	for bgs in ['','_bgs']:
@@ -69,7 +75,8 @@ for f in glob.glob('*.img'):
 		else:
 			plt.figure(figsize=(7.5/2.54,5.3/2.54))
 
-		plt.pcolormesh(int2d.radial,int2d.azimuthal,int2d.intensity,cmap='coolwarm')
+		plt.pcolormesh(int2d.radial,int2d.azimuthal,int2d.intensity,cmap='coolwarm',norm=mpl.colors.LogNorm(vmin=1e-1
+		) if 'SAXS' in filename else None)
 
 		plt.xlabel(label(int2d.radial_unit));plt.ylabel(label(int2d.azimuthal_unit))
 		plt.tick_params(axis='both',pad=2,labelsize=8)
@@ -91,6 +98,8 @@ for f in glob.glob('*.img'):
 
 		plt.plot(int1d.radial,int1d.intensity,'k',linewidth=0.5)
 
+		if 'SAXS' in filename:
+			plt.yscale('log');plt.ylim([1e-1,None])
 		plt.xlabel(label(int1d.unit));plt.ylabel(r'$I/1$')
 		plt.tick_params(axis='both',pad=2,labelsize=8)
 		plt.tight_layout(pad=0.1)
