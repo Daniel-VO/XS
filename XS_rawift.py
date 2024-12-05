506dac90f3feb1528a3b4f2dd19cdc7941063fd4
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-12-05 08:15:44 +0100

    Add files via upload

--- XS_rawift.py
+++ XS_rawift.py
@@ -1,5 +1,5 @@
 """
-Created 29. November 2024 by Daniel Van Opdenbosch, Technical University of Munich
+Created 05. Dezember 2024 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -8,9 +8,17 @@ import os
 import sys
 import glob
 import numpy as np
+import jscatter as js
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import bioxtasraw.RAWAPI as raw
+
+f=10;s=10;lamb=1.5406;rGon=300;d1=173.5;d2=2*rGon-d1
+AHlen=(f/2+s/2)/d1*2*rGon
+LHlen=(f/2+s/2)/d1*d2
+a=4*np.pi*np.sin(np.arctan(AHlen/rGon/2))/lamb
+b=4*np.pi*np.sin(np.arctan(LHlen/rGon/2))/lamb
+dIW=4*np.pi*np.sin(np.arctan((AHlen-LHlen)/rGon/2))/lamb
 
 ranges=[
 		[7e-3,3e-1],
@@ -18,7 +26,7 @@ ranges=[
 
 os.system('mv results.log results.alt')
 for r in ranges:
-	for f in glob.glob('**/*.dat'):
+	for f in glob.glob('*.dat'):
 		filename=os.path.splitext(f)[0]+'_'+str(r)
 		if not os.path.exists(filename+'.pdf'):
 			print(filename)
@@ -28,14 +36,20 @@ for r in ranges:
 			q,yobs=q[args],yobs[args]											####
 			q,yobs=q[np.argmax(yobs):],yobs[np.argmax(yobs):]					####
 
+			plt.close('all')
+			plt.plot(q,yobs)
+
+			bxw=float(open(f).readlines()[0].split('=')[-1])
+			if isinstance(bxw,float):
+				print('Schlitzkorrektur mit Werten a b bxw dIW: ',round(a,4),round(b,4),round(bxw,4),round(dIW,4))
+				dsm=js.sas.desmear(js.dA(np.array([q,yobs])),js.sas.prepareBeamProfile('trapez',a=a,b=b,bxw=bxw,dIW=dIW))
+				q,yobs=dsm.X,dsm.Y
+
 			args=np.where((q>=r[0])&(q<=r[1]))									####
 			profile=raw.make_profile(q[args],yobs[args],np.ones(len(q[args])),filename)
-			ift=raw.bift(profile)
-
-			profile=raw.make_profile(q,yobs,np.ones(len(q)),filename)
 			guinier=raw.auto_guinier(profile)
+			ift=raw.datgnom(profile)
 
-			plt.close('all')
 			plt.plot(q,yobs)
 			plt.plot(ift[0].q_orig,ift[0].i_orig)
 			plt.plot(ift[0].q_orig,ift[0].i_fit)
