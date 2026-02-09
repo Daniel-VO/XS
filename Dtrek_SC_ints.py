d421ec478f342fc5b9c26a5600a459767369f897
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2026-02-09 10:31:20 +0100

    Delete Dtrek_SC_ints.py

--- Dtrek_SC_ints.py
+++
@@ -1,8 +0,0 @@
-import glob
-import fabio
-import numpy as np
-import matplotlib.pyplot as plt
-
-plt.plot([np.sum(fabio.open(i).data) for i in sorted(glob.glob('*[!alle].img'))])
-plt.ylim([0,None])
-plt.savefig('SC_ints.png')
