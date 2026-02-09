18d9e6f7637a65fd89a2cf4bbf46f337742e2ba4
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2026-02-09 10:22:03 +0100

    Delete Dtrek_int_tPLc.py

--- Dtrek_int_tPLc.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-import numpy as np
-
-for f in glob.glob('*.xy'):
-	if '2th_deg' in open(f,'r').read():
-		fn=os.path.splitext(f)[0]
-		tt,yobs=np.genfromtxt(f,unpack=True,skip_header=24)
-		np.savetxt('Profex/'+fn+'_profex.xy',np.transpose([tt,yobs/np.sin(np.radians(tt))]),fmt='%.8f')
