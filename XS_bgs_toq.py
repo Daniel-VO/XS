6a67f0dc6152b20725f056448581f80c5bdbd280
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2026-04-07 11:23:03 +0200

    Add files via upload

---
+++ XS_bgs_toq.py
@@ -0,0 +1,105 @@
+"""
+Created 10. Februar 2026 by Daniel Van Opdenbosch, Technical University of Munich
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
+"""
+
+import os
+import ray
+import glob
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+
+def toq(tt_deg):
+	return 4*np.pi*np.sin(np.radians(tt_deg/2))/1.5406
+
+def gaussian(x,a,x0,sigma):
+	return a*np.exp(-((x-x0)/sigma)**2/2)
+
+def gaussfit(q,y):
+	args=q<=-min(q)
+	params,_=scipy.optimize.curve_fit(gaussian,q[args],y[args],p0=[max(y),0,1e-3])
+	return params
+
+os.system('rm *_toq.png')
+os.system('rm *_toq.dat')
+
+@ray.remote
+def corr(f,bgfiles):
+	fn=os.path.splitext(f)[0]
+
+	for line in open(f,'r').readlines():
+		if '*HW_XG_WAVE_LENGTH_ALPHA1' in line:
+			lamb=float(eval(line.split(' ')[-1]))
+		if '*HW_GONIOMETER_RADIUS-3' in line:
+			rGon=float(eval(line.split(' ')[-1]))
+		if '*HW_GONIOMETER_RADIUS-2' in line:
+			d1=float(eval(line.split(' ')[-1]))
+		if '*HW_XG_FOCUS "' in line:
+			fs=float(eval(line.split('x')[-1].split('mm')[0]))
+		if '"LLS"' in line:
+			llsnumber=eval(line.split(' ')[0].split('-')[-1])
+	for line in open(f,'r').readlines():
+		if '*MEAS_COND_AXIS_POSITION-'+str(llsnumber) in line:
+			ss=float(eval(line.split(' ')[-1]).split('mm')[0])
+	d2=2*rGon-d1
+	AHlen=(fs/2+ss/2)/d1*2*rGon
+	LHlen=(fs/2+ss/2)/d1*d2
+	a=4*np.pi*np.sin(np.arctan(AHlen/2/rGon))/lamb
+	b=4*np.pi*np.sin(np.arctan(LHlen/2/rGon))/lamb
+	dIW=4*np.pi*np.sin(np.arctan((AHlen-LHlen)/2/rGon))/lamb
+
+	tt_deg,yobs,eps=np.genfromtxt((i.replace('*','#') for i in open(f)),unpack=True)
+	yobs/=1+np.cos(np.radians(tt_deg))**2
+	q=toq(tt_deg);bgs=''														#to q
+
+	if len(bgfiles)==0:
+		print('Kein Untergrund')
+		HWHM=0
+		ybg=np.zeros(yobs.shape)
+	else:
+		bgstrings=['_SAXS','_USAXS']
+		for bgstring in bgstrings:
+			if bgstring in fn:
+				bgfile=bgfiles[[i for i,l in enumerate(bgfiles) if bgstring in l][0]]
+			else:
+				bgfile=bgfiles[[i for i,l in enumerate(bgfiles) if bgstring not in l][0]]
+
+		ttbg_deg,ybg,epsbg=np.genfromtxt((i.replace('*','#') for i in open(bgfile)),unpack=True)
+		ybg/=1+np.cos(np.radians(ttbg_deg))**2
+		qbg=toq(ttbg_deg)														#to q
+		if min(q)<0:
+			gpm=gaussfit(q,yobs);gpb=gaussfit(qbg,ybg)
+			q-=gpm[1];qbg-=gpb[1]												#zdc
+			bxw=(2*np.log(2))**0.5*abs(gpb[2])									#HWHM
+		else:
+			bxw=qbg[np.where(ybg<max(ybg)/2)[0][0]]
+
+		argscut=(q>=min(qbg))&(q<=max(qbg))
+		q=q[argscut];yobs=yobs[argscut]											#cut
+		ybg=scipy.interpolate.interp1d(qbg,ybg)(q)
+		yobs-=ybg/gpb[0]*gpm[0];bgs='_bgs'										#bgcorr
+
+		plt.close('all')
+		plt.plot(q,yobs+ybg);plt.plot(q,ybg);plt.plot(q,yobs)
+		plt.plot(np.linspace(q[0],-q[0]),gaussian(np.linspace(q[0],-q[0]),gpm[0],0,gpm[2]))
+		plt.plot(np.linspace(q[0],-q[0]),gaussian(np.linspace(q[0],-q[0]),gpb[0],0,gpb[2]))
+		plt.yscale('log');plt.xlim([q[0]*1.02,-q[0]*1.02]);plt.ylim([(yobs+ybg)[0]/1.02,max(gaussian(np.linspace(q[0],-q[0]),*gpb))*1.02])
+		plt.savefig(fn+'_cb.png')
+
+	plt.close('all')
+	plt.plot(q,yobs)
+	if len(bgfiles)!=0:
+		plt.plot(q,yobs+ybg);plt.plot(q,ybg)
+		plt.figtext(0.98,0.98,'BG: '+os.path.splitext(bgfile)[0],ha='right',va='top')
+	plt.xscale('log');plt.yscale('log')
+	plt.tight_layout(pad=0.1)
+	plt.savefig(fn+'.png')
+
+	with open(fn+bgs+'_toq.dat','a') as d:
+		print('#'+str({'a':a,'b':b,'bxw':bxw,'dIW':dIW}),file=d)
+		np.savetxt(d,np.transpose([q,yobs]),fmt='%.8f')
+
+bgfiles=glob.glob('*_BG.ras')
+ray.get([corr.remote(f,bgfiles) for f in glob.glob('*.ras')])
--- XS_svfit.py
+++ XS_svfit.py
@@ -1,5 +1,5 @@
 """
-Created 03. Dezember 2025 by Daniel Van Opdenbosch, Technical University of Munich
+Created 27. Maerz 2025 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -20,22 +20,6 @@ from sasmodels.direct_model import call_kernel
 
 def pad(q):
 	return np.concatenate((np.linspace(min(q)/2,min(q)),q,np.linspace(max(q),max(q)*2)))
-
-def fitfunc(params):
-	prm=params.valuesdict()
-	global res
-	res=((yobsS-SAXSres.apply(call_kernel(SAXSkernel,funcy.omit(prm,['Uscale','Ubackground'])))[50:-50])*qS**1)
-	if os.path.isfile(f.replace('_SAXS','_USAXS')):
-		prm['scale']=prm['Uscale'];prm['background']=prm['Ubackground']
-		res=np.append(((yobsU-USAXSres.apply(call_kernel(USAXSkernel,funcy.omit(prm,['Uscale','Ubackground'])))[50:-50])*qU**1),res)
-	return np.nan_to_num(res)
-
-f=10;s=10;lamb=1.5406;rGon=300;d1=173.5;d2=2*rGon-d1
-AHlen=(f/2+s/2)/d1*2*rGon
-LHlen=(f/2+s/2)/d1*d2
-a=4*np.pi*np.sin(np.arctan(AHlen/2/rGon))/lamb
-b=4*np.pi*np.sin(np.arctan(LHlen/2/rGon))/lamb
-dIW=4*np.pi*np.sin(np.arctan((AHlen-LHlen)/2/rGon))/lamb
 
 model=load_model(str(sys.argv[1]))
 params=lm.Parameters()
@@ -59,32 +43,24 @@ else:
 os.system('mv '+model.info.id+'.log '+model.info.id+'.alt')
 
 @ray.remote
-def fit(g):
-	filename=g.split('_SAXS')[0]
-	global f,qS,yobsS,SAXSres,SAXSkernel;f=g
-	qS,yobsS=np.genfromtxt(f,unpack=True)
-	q_widthS=float(open(f).readlines()[0].split('=')[-1])
+def fit(f):
+	fn=os.path.splitext(f)[0]
+	q,yobs=np.genfromtxt(f,unpack=True)
+	slinf=eval(open(f).readlines()[0].split('#')[-1])
 
-	# ~ qS,yobsS=qS[qS>1e-2],yobsS[qS>1e-2]						####
+	q,yobs=q[q>7e-3],yobs[q>7e-3]												####
 
-	if os.path.isfile(f.replace('_SAXS','_USAXS')):
-		params.add('Uscale',params.valuesdict()['scale']/10,min=0);params.add('Ubackground',0,min=0)
-		global qU,yobsU,USAXSres,USAXSkernel
-		qU,yobsU=np.genfromtxt(f.replace('_SAXS','_USAXS'),unpack=True)
-		q_widthU=float(open(f.replace('_SAXS','_USAXS')).readlines()[0].split('=')[-1])
+	qpad=pad(q)
+	smear=Slit1D(qpad,q_length=slinf['dIW'],q_width=slinf['bxw'],q_calc=qpad)
+	kernel=model.make_kernel([qpad])
 
-		# ~ qU,yobsU=qU[qU>1e-3],yobsU[qU>1e-3]					####
-
-		qUkernel=pad(qU)
-		USAXSres=Slit1D(qUkernel,q_length=dIW,q_width=q_widthU,q_calc=qUkernel)
-		USAXSkernel=model.make_kernel([qUkernel])
-
-	qSkernel=pad(qS)
-	SAXSres=Slit1D(qSkernel,q_length=dIW,q_width=q_widthS,q_calc=qSkernel)
-	SAXSkernel=model.make_kernel([qSkernel])
+	def fitfunc(params):
+		prm=params.valuesdict();global res
+		res=((yobs-smear.apply(call_kernel(kernel,funcy.omit(prm,['Uscale','Ubackground'])))[50:-50])*q**1)
+		return np.nan_to_num(res)
 
 	result=lm.minimize(fitfunc,params,method='leastsq')
-	print(filename,sys.argv[1])
+	print(fn,sys.argv[1])
 	result.params.pretty_print()
 	prm=result.params.valuesdict()
 
@@ -93,13 +69,8 @@ def fit(g):
 	mpl.rc('text.latex',preamble=r'\usepackage[helvet]{sfmath}')
 	plt.figure(figsize=(7.5/2.54,5.3/2.54))
 
-	plt.plot(qS,yobsS,'.',ms=1,color='k')
-	plt.plot(qSkernel[50:-50],SAXSres.apply(call_kernel(SAXSkernel,funcy.omit(prm,['Uscale','Ubackground'])))[50:-50],'0.3',linewidth=0.5)
-
-	if os.path.isfile(f.replace('_SAXS','_USAXS')):
-		prm['scale']=prm['Uscale'];prm['background']=prm['Ubackground']
-		plt.plot(qU,yobsU,'.',color='k')
-		plt.plot(qUkernel[50:-50],USAXSres.apply(call_kernel(USAXSkernel,funcy.omit(prm,['Uscale','Ubackground'])))[50:-50],'0.3',linewidth=0.5)
+	plt.plot(q,yobs,'.',ms=1,color='k')
+	plt.plot(qpad[50:-50],smear.apply(call_kernel(kernel,funcy.omit(prm,['Uscale','Ubackground'])))[50:-50],'0.3',linewidth=0.5)
 
 	plt.xscale('log');plt.yscale('log')
 
@@ -107,16 +78,21 @@ def fit(g):
 	plt.ylabel(r'$I/1$',fontsize=10)
 	plt.tick_params(axis='both',pad=2,labelsize=8)
 	plt.tight_layout(pad=0.1)
-	plt.savefig(filename+'_'+model.info.id+'.png',dpi=300)
+	plt.savefig(fn+'_'+model.info.id+'.png',dpi=300)
 
-	# ~ plt.close('all')
-	# ~ plt.plot(qU,res[:len(qU)])
-	# ~ plt.plot(qS,res[len(qU):])
-	# ~ plt.xscale('log')
-	# ~ plt.savefig(filename+'_'+model.info.id+'_res.png',dpi=300)
+	if model.info.id=='unified_power_Rg':
+		prm0=result.params
+		result.params=lm.Parameters()
+		lnums=sum(['rg' in k for k  in prm0.keys()])
+		sortkey=np.argsort([prm0['rg'+str(level+1)] for level in range(lnums)])
+		for level in range(lnums):
+			result.params['rg'+str(level+1)]=prm0['rg'+str(sortkey[level]+1)]
+			result.params['power'+str(level+1)]=prm0['power'+str(sortkey[level]+1)]
+			result.params['B'+str(level+1)]=prm0['B'+str(sortkey[level]+1)]
+			result.params['G'+str(level+1)]=prm0['G'+str(sortkey[level]+1)]
 
-	print(filename,[str((param.name,'=',param.value,'+-',param.stderr)) for param in result.params.values()],np.sum(res**2),file=open(model.info.id+'.log','a'))
+	print(fn,[str((param.name,'=',param.value,'+-',param.stderr)) for param in result.params.values()],np.sum(res**2),file=open(model.info.id+'.log','a'))
 
-	return filename,result
+	return fn,result
 
-np.save(model.info.id+'.npy',ray.get([fit.remote(g) for g in glob.glob('*_SAXS*.dat')]))
+np.save(model.info.id+'.npy',ray.get([fit.remote(f) for f in glob.glob('*.dat')]))
