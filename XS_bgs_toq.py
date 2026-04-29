579035b9aabb73ae1477b24df949b162fbd998bc
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2026-04-29 15:20:59 +0200

    Add files via upload

--- XS_bgs_toq.py
+++ XS_bgs_toq.py
@@ -20,59 +20,60 @@ def gaussfit(q,y):
 	params,_=scipy.optimize.curve_fit(gaussian,q[args],y[args],p0=[max(y),0,1e-3])
 	return params
 
-Mleer=input('Haltermessung, .ras: ')
+bg=input('Haltermessung, .ras: ')
 
-ttbg_deg,ybg,epsbg=np.genfromtxt((i.replace('*','#') for i in open(Mleer)),unpack=True)
+ttbg_deg,ybg,epsbg=np.genfromtxt((i.replace('*','#') for i in open(bg)),unpack=True)
 
 @ray.remote
 def corr(f):
-	fn=os.path.splitext(f)[0]
+	if f!=bg:
+		fn=os.path.splitext(f)[0]
 
-	for line in open(f,'r').readlines():
-		if '*HW_XG_WAVE_LENGTH_ALPHA1' in line:
-			lamb=float(eval(line.split(' ')[-1]))
-		if '*HW_GONIOMETER_RADIUS-3' in line:
-			rGon=float(eval(line.split(' ')[-1]))
-		if '*HW_GONIOMETER_RADIUS-2' in line:
-			d1=float(eval(line.split(' ')[-1]))
-		if '*HW_XG_FOCUS "' in line:
-			fs=float(eval(line.split('x')[-1].split('mm')[0]))
-		if '"LLS"' in line:
-			llsnumber=eval(line.split(' ')[0].split('-')[-1])
-	for line in open(f,'r').readlines():
-		if '*MEAS_COND_AXIS_POSITION-'+str(llsnumber) in line:
-			ss=float(eval(line.split(' ')[-1]).split('mm')[0])
+		for line in open(f,'r').readlines():
+			if '*HW_XG_WAVE_LENGTH_ALPHA1' in line:
+				lamb=float(eval(line.split(' ')[-1]))
+			if '*HW_GONIOMETER_RADIUS-3' in line:
+				rGon=float(eval(line.split(' ')[-1]))
+			if '*HW_GONIOMETER_RADIUS-2' in line:
+				d1=float(eval(line.split(' ')[-1]))
+			if '*HW_XG_FOCUS "' in line:
+				fs=float(eval(line.split('x')[-1].split('mm')[0]))
+			if '"LLS"' in line:
+				llsnumber=eval(line.split(' ')[0].split('-')[-1])
+		for line in open(f,'r').readlines():
+			if '*MEAS_COND_AXIS_POSITION-'+str(llsnumber) in line:
+				ss=float(eval(line.split(' ')[-1]).split('mm')[0])
 
-	AHlen,LHlen=(fs/2+ss/2)/d1*np.array([2*rGon,2*rGon-d1])
-	a,b,dIW=4*np.pi*np.sin(np.arctan(np.array([AHlen,LHlen,AHlen-LHlen])/2/rGon))/lamb
+		AHlen,LHlen=(fs/2+ss/2)/d1*np.array([2*rGon,2*rGon-d1])
+		a,b,dIW=4*np.pi*np.sin(np.arctan(np.array([AHlen,LHlen,AHlen-LHlen])/2/rGon))/lamb
 
-	tt_deg,yobs,eps=np.genfromtxt((i.replace('*','#') for i in open(f)),unpack=True)
-	global ttbg_deg,ybg
-	q,qbg=4*np.pi*np.sin(np.radians(np.array([tt_deg,ttbg_deg])/2))/lamb		#to q
+		tt_deg,yobs,eps=np.genfromtxt((i.replace('*','#') for i in open(f)),unpack=True)
+		global ttbg_deg,ybg
+		q,qbg=4*np.pi*np.sin(np.radians(np.array([tt_deg,ttbg_deg])/2))/lamb		#to q
 
-	if min(q)<0:
-		gpm=gaussfit(q,yobs);gpb=gaussfit(qbg,ybg)
-		q-=gpm[1];qbg-=gpb[1]													#zdc
-		bxw=(2*np.log(2))**0.5*abs(gpb[2])										#HWHM
-	else:
-		bxw=qbg[np.where(ybg<max(ybg)/2)[0][0]]
+		if min(q)<0:
+			gpm=gaussfit(q,yobs);gpb=gaussfit(qbg,ybg)
+			q-=gpm[1];qbg-=gpb[1]													#zdc
+			bxw=(2*np.log(2))**0.5*abs(gpb[2])										#HWHM
+		else:
+			bxw=qbg[np.where(ybg<max(ybg)/2)[0][0]]
 
-	argscut=(q>=min(qbg))&(q<=max(qbg))
-	q=q[argscut];yobs=yobs[argscut]												#cut
-	ybg=scipy.interpolate.interp1d(qbg,ybg)(q)
-	yobs-=ybg/gpb[0]*gpm[0]														#bgcorr
+		argscut=(q>=min(qbg))&(q<=max(qbg))
+		q=q[argscut];yobs=yobs[argscut]												#cut
+		ybg=scipy.interpolate.interp1d(qbg,ybg)(q)
+		yobs-=ybg/gpb[0]*gpm[0]														#bgcorr
 
-	yobs/=(1+np.cos(np.radians(tt_deg[argscut]))**2)/2							#polcorr
+		yobs/=(1+np.cos(np.radians(tt_deg[argscut]))**2)/2							#polcorr
 
-	plt.close('all')
-	plt.plot(q,yobs)
-	plt.xscale('log');plt.yscale('log')
-	plt.tight_layout(pad=0.1)
-	plt.savefig(fn+'.png')
+		plt.close('all')
+		plt.plot(q,yobs)
+		plt.xscale('log');plt.yscale('log')
+		plt.tight_layout(pad=0.1)
+		plt.savefig(fn+'.png')
 
-	os.system('rm *_toq.dat')
-	with open(fn+'_bgs_toq.dat','a') as d:
-		print('# '+str({'a':a,'b':b,'bxw':bxw,'dIW':dIW}),file=d)
-		np.savetxt(d,np.transpose([q,yobs]),fmt='%.8f')
+		os.system('rm '+fn+'.dat')
+		with open(fn+'.dat','a') as d:
+			print('# '+str({'a':a,'b':b,'bxw':bxw,'dIW':dIW}),file=d)
+			np.savetxt(d,np.transpose([q,yobs]),fmt='%.8f')
 
 ray.get([corr.remote(f) for f in glob.glob('*.ras')])
