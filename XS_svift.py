3859b6be5d83757862352cd13d0f62a9b9499245
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2026-05-20 08:12:40 +0200

    Add files via upload

--- XS_svift.py
+++ XS_svift.py
@@ -29,32 +29,26 @@ for ran in ranges:
 		filename=os.path.splitext(f)[0]+'_'+str(ran)
 		print(filename)
 		q,yobs=np.genfromtxt(f,unpack=True)
+		slinf=eval(open(f).readlines()[0].split('#')[-1])
+		invertor=Invertor()
 
 		args=(q>=ran[0])&(q<=ran[1])											####
 		q,yobs=q[args],yobs[args]												####
 		q,yobs=q[np.argmax(yobs):],yobs[np.argmax(yobs):]						####
+		invertor.set_dmax(2*np.pi/min(q))										####
+		invertor.alpha=0														####
+		invertor.nfunc=20														####
 
-		plt.close('all')
-
-		slinf=eval(open(f).readlines()[0].split('#')[-1])
-		invertor=Invertor()
-		invertor.set_x(q);invertor.set_y(yobs);invertor.set_err(np.ones(len(q)))
-		invertor.set_slit_height(slinf['dIW']);invertor.set_slit_width(slinf['bxw'])
-
-		out,cov=invertor.invert(nfunc=int(invertor.get_dmax()/12))
-		r=np.arange(0.0,invertor.d_max,invertor.d_max/100)
+		invertor.x=q;invertor.y=yobs;invertor.err=np.ones(len(q))
+		if 'dIW' in slinf.keys():
+			invertor.slit_height=slinf['dIW'];invertor.slit_width=slinf['bxw']
+		else:
+			invertor.slit_height=slinf['bxw'];invertor.slit_width=slinf['bxw']
+		out,cov=invertor.invert()
+		r=np.linspace(0,invertor.d_max,num=len(q))
 		pr=invertor.pr_err(out,cov,r)
 
-		for _ in range(10):
-			print(np.any(pr[0]<0),invertor.get_dmax())
-			if np.any(pr[0]<0):
-				invertor.set_dmax(1.2*r[pr[0]<0][0])
-			else:
-				invertor.set_dmax(1.2*invertor.get_dmax())
-
-			out,cov=invertor.invert(nfunc=int(invertor.get_dmax()/12))
-			r=np.arange(0.0,invertor.d_max,invertor.d_max/100)
-			pr=invertor.pr_err(out,cov,r)
+		plt.close('all')
 
 		plt.plot(q,yobs)
 		plt.plot(q,invertor.get_iq_smeared(out,q))
@@ -62,7 +56,8 @@ for ran in ranges:
 		plt.savefig(filename+'_iq.png')
 
 		plt.close('all')
-		plt.errorbar(r,pr[0],yerr=pr[1])
+		plt.plot(r,pr[0])
+		plt.fill_between(r,pr[0]+pr[1]/2,pr[0]-pr[1]/2,alpha=0.5)
 		plt.savefig(filename+'_pr.png')
 
 		invertor.to_file(filename+'_pr.txt')
